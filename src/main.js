ffeda38fd4d25fe0bc2aaf4b6174a02fd5b4b9be
Odagawa <xtctl.1234@gmail.com>
2020-06-22 20:33:44 +0900

    add fonts

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import whimClientVue from "whim-client-vue";
 import "whim-client-vue/dist/whim-client-vue.css";
+import "./assets/main.scss";
 
 Vue.config.productionTip = false;
 Vue.use(whimClientVue);
